d3119156719f0fcaf508985670828515698a8a78
Mayank fulera <fuleramayank@gmail.com>
2022-09-21 14:25:25 +0530

    vue

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+@import "@cldcvr/flow-core/dist/style.css";
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <f-button label="label"></f-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
