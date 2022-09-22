6774d2bf50133846547413c793fea129124e2f01
mayankfulera <fuleramayank@gmail.com>
2022-09-22 12:01:25 +0530

    FLOW-406 Vue (#2)
    * vue
    
    * vue
    
    * changes
    
    * changes

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <f-div width="100%" height="100%" state="default">
+    <router-view />
+  </f-div>
+</template>
+
+<style>
+@import "@cldcvr/flow-core/dist/style.css";
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@cldcvr/flow-core/dist/types/vue3";
+import "@cldcvr/flow-core";
+import "@cldcvr/flow-system-icon";
+
+createApp(App).use(router).mount("#app");
