1b55dc4dbfb79fda59a9e45ec601033d72557259
mayankfulera <fuleramayank@gmail.com>
2022-10-10 15:14:28 +0530

    FLOW-425 New UI changes for Vue Starter Kit (#3)
    * vue
    
    * vue
    
    * changes
    
    * changes
    
    * changes
    
    * update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
-  <f-button label="label"></f-button>
+  <LayoutView>
+    <BodySection></BodySection>
+  </LayoutView>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import BodySection from "@/components/BodySection.vue";
+import LayoutView from "../components/LayoutView.vue";
 
 export default defineComponent({
   name: "HomeView",
+  components: { BodySection, LayoutView },
 });
 </script>
