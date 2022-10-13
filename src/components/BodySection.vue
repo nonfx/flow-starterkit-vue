78100ae1972be7009ba53f99b5733d8eebcc4a15
vikas-cldcvr <vikas@cldcvr.com>
2022-10-13 17:40:47 +0530

    FLOW-430 landing page links updated

--- src/components/BodySection.vue
+++ src/components/BodySection.vue
@@ -82,7 +82,7 @@
               </f-text>
             </f-div>
             <f-div padding="none" align="top-left">
-              <a :href="item?.link1?.link" target="blank">
+              <a :href="item?.link1?.link" target="_blank">
                 <f-text
                   variant="para"
                   size="small"
@@ -94,7 +94,7 @@
               </a>
             </f-div>
             <f-div padding="none" align="top-left">
-              <a :href="item?.link2?.link" target="blank">
+              <a :href="item?.link2?.link" target="_blank">
                 <f-text
                   variant="para"
                   size="small"
@@ -106,7 +106,7 @@
               </a>
             </f-div>
             <f-div padding="none" align="top-left" v-if="item?.link3">
-              <a :href="item?.link1?.link" target="blank">
+              <a :href="item?.link1?.link" target="_blank">
                 <f-text
                   variant="para"
                   size="small"
@@ -138,26 +138,50 @@ export default defineComponent({
         title: "Component library",
         subTitle:
           "Discover all Flow components, their usage in a storybook library.",
-        link1: { name: "f-button", link: "" },
-        link2: { name: "f-icon", link: "" },
-        link3: { name: "f-pictogram", link: "" },
+        link1: {
+          name: "f-button",
+          link: "https://flow.cldcvr.com/v2/index.html?path=/docs/components-f-button--playground",
+        },
+        link2: {
+          name: "f-icon",
+          link: "https://flow.cldcvr.com/v2/index.html?path=/docs/components-f-icon--anatomy",
+        },
+        link3: {
+          name: "f-pictogram",
+          link: "https://flow.cldcvr.com/v2/index.html?path=/docs/components-f-pictogram--anatomy",
+        },
       },
       {
         icon: "i-table-1",
         title: "Templates",
         subTitle:
           "Find all the useful templates with their code in a single storybook.",
-        link1: { name: "top-nav", link: "" },
-        link2: { name: "side-nav", link: "" },
-        link3: { name: "slab", link: "" },
+        link1: {
+          name: "top-nav",
+          link: "https://flow.cldcvr.com/templates/index.html?path=/docs/top-nav--anatomy",
+        },
+        link2: {
+          name: "side-nav",
+          link: "https://flow.cldcvr.com/templates/index.html?path=/docs/top-nav--anatomy",
+        },
+        link3: {
+          name: "slab",
+          link: "https://flow.cldcvr.com/templates/index.html?path=/docs/top-nav--anatomy",
+        },
       },
       {
         icon: "i-git",
         title: "Dependencies",
         subTitle:
           "Consume what you need by selecting logically grouped dependencies.",
-        link1: { name: "Flow system icon", link: "" },
-        link2: { name: "Flow product icon", link: "" },
+        link1: {
+          name: "Flow system icon",
+          link: "https://flow.cldcvr.com/v2/index.html?path=/docs/dependencies-icons--system-icons",
+        },
+        link2: {
+          name: "Flow product icon",
+          link: "https://flow.cldcvr.com/v2/index.html?path=/docs/dependencies-icons--product-icons",
+        },
         link3: { name: "Flow forms (coming soon)", link: "" },
       },
       {
@@ -190,10 +214,14 @@ export default defineComponent({
 .bottom-section::-webkit-scrollbar {
   display: none;
 }
+
 .bottom-section {
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none;
+  /* IE and Edge */
+  scrollbar-width: none;
+  /* Firefox */
 }
+
 .bottom-section a {
   text-decoration: none;
 }
