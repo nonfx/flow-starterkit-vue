9a70f1dccfae333d58241bb0bc3dc7a0063be358
vikas-cldcvr <vikas@cldcvr.com>
2023-01-09 17:15:55 +0530

    :art: flow im[port synatx updated

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@cldcvr/flow-core/dist/types/vue3";
-import "@cldcvr/flow-core";
-import "@cldcvr/flow-system-icon";
-import "@cldcvr/flow-product-icon";
 
-createApp(App).use(router).mount("#app");
+import("@cldcvr/flow-core").then(async () => {
+  await import("@cldcvr/flow-system-icon");
+  await import("@cldcvr/flow-product-icon");
+  createApp(App).use(router).mount("#app");
+});
